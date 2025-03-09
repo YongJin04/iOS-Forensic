cb555a89d7cc3207dc5ca355fab7a593d05b504a
YongJin04 <jky8130@gmail.com>
2025-03-09 18:23:59 +0900

    Create iOS backup analyzer.

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,11 +1,28 @@
+# main_window.py
 import tkinter as tk
 from tkinter import ttk
+import os
+import sys
+
 from gui.gui_layout import setup_gui
 
+# Windows에서 작업 표시줄 아이콘을 변경하기 위한 모듈
+if sys.platform == "win32":
+    import ctypes
+
 def start_gui():
-    """ GUI 실행 함수 (반응형 적용) """
+    """ GUI 실행 함수 (반응형 적용 + 작업 표시줄 아이콘 변경) """
     root = tk.Tk()
-    root.title("iTunes Backup Viewer")
+    root.title("iOS Forensic Viewer")
+
+    # 아이콘 경로 설정
+    icon_path_ico = os.path.abspath(os.path.join("gui", "icon", "pay1oad.ico"))
+
+    # Windows 작업 표시줄 아이콘 설정
+    if sys.platform == "win32":
+        if os.path.exists(icon_path_ico):
+            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("iOS.Forensic.Viewer")
+            root.iconbitmap(icon_path_ico)
 
     # 최소 크기 제한 (너무 작아지지 않도록)
     root.minsize(650, 400)
